6e84f42331ab638d7a7b1141cc78bc5a1d72f83f
sinqx <sinqxpls@gmail.com>
2023-12-14 16:04:51 +0600

    test

--- ebazar/urls.py
+++ ebazar/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 from myapp import views
 
 urlpatterns = [
-    path('', include('myapp.urls')),
+    path("myapp/", views.index),
     path('admin/', admin.site.urls),
 ]
--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import signup
+
+from . import views
 
 urlpatterns = [
-    path('signup/', signup, name='signup'),
+    path("", views.index, name="index"),
 ]
