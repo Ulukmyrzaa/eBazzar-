be3388432cc71228b514ef90ffbec1e08ea11736
akichun97 <akichun97@gmail.com>
2023-12-14 17:23:15 +0600

    frontend

--- ebazar/urls.py
+++ ebazar/urls.py
@@ -18,7 +18,11 @@ from django.contrib import admin
 from django.urls import path, include
 from myapp import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path("myapp/", views.index),
     path('admin/', admin.site.urls),
+    path("", include("myapp.urls"))
 ]
